7c81d8076fd79618c467235bb0da9a8f07bc548d
elena <kronverk01@gmail.com>
2026-03-07 20:45:52 +0000

    add CORS

--- backend/src/main/java/com/example/referraltracker/controller/HelloController.java
+++ backend/src/main/java/com/example/referraltracker/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.petproject;
+package com.example.referraltracker;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = {
         "http://localhost:5173",
-        "https://referral-tracker-frontend.onrender.com"
+        "https://referral-tracker-fe.onrender.com"
 })
 public class HelloController {
 
