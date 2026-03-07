ae3da84ba9b72f5cbde7001a5d12087efd13e37c
elena <kronverk01@gmail.com>
2026-03-07 15:01:12 +0000

    Initial project structure: backend and frontend

---
+++ backend/src/main/java/com/example/petproject/ReferralTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.example.petproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferralTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReferralTrackerApplication.class, args);
+    }
+}
---
+++ frontend/referral_tracker/src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/hello")
+            .then(res => res.text())
+            .then(data => setMessage(data));
+    }, []);
+
+    return (
+        <div style={{textAlign:"center", marginTop:"100px"}}>
+            <h1>Backend says:</h1>
+            <h2>{message}</h2>
+        </div>
+    );
+}
+
+export default App;
